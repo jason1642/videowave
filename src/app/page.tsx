af28f4e4f5a7c7815f09e3d3d8ab5de33c2ee02b
jason1642 <jason1642@gmail.com>
2024-02-21 14:39:40 -0500

    Python/postgres server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 'use client'
 import Image from "next/image";
 import React from 'react'
-import { useRef } from 'react'
-// import StoreProvider from "./StoreProvider";
-// import AuthUpdater from "./auth-updater";
-// import AuthViewer from "./auth-viewer";
+
 
 
 export default function Home() {
 
 
   return (
-    // <StoreProvider>
+
 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -120,6 +117,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-    // </StoreProvider>
   );
 }
